8f960343281efaaa197aeb023395226e81b1a0c5
maxixking66 <maxixking66@gamil.com>
2025-04-18 09:44:10 +0330

    feat: add example 3 for managed entity by merge method

--- src/main/java/ir/maktabsharif127/jpa/JpaApplication.java
+++ src/main/java/ir/maktabsharif127/jpa/JpaApplication.java
@@ -13,9 +13,19 @@ public class JpaApplication {
 
         entityManager.getTransaction().begin();
 
-        Tag tag = entityManager.find(Tag.class, 1L);
+        Tag tag = new Tag();
+        tag.setName("new with merge 2");
+        tag.setIsActive(true);
 
-        tag.setName("second");
+        System.out.println("before merge: " + entityManager.contains(tag));
+
+        Tag merge = entityManager.merge(tag);/*managed*/
+//        tag = entityManager.merge(tag);/*managed*/
+
+        System.out.println("after merge: " + entityManager.contains(tag));
+        System.out.println("after merge and return type of merge method: " + entityManager.contains(merge));
+        merge.setName("merge return type");
+//        entityManager.persist(tag);
 
         entityManager.getTransaction().commit();
 
