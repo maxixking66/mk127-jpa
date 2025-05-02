5c97e448a0c5a5ad74c67f3a4717853032230ee7
maxixking66 <maxixking66@gamil.com>
2025-05-02 11:53:59 +0330

    add proxy example

--- src/main/java/ir/maktabsharif127/jpa/JpaApplication.java
+++ src/main/java/ir/maktabsharif127/jpa/JpaApplication.java
@@ -1,23 +1,84 @@
 package ir.maktabsharif127.jpa;
 
-import ir.maktabsharif127.jpa.config.ApplicationContext;
-import ir.maktabsharif127.jpa.designpatters.User;
-import ir.maktabsharif127.jpa.repository.ProvinceRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class JpaApplication {
-
     public static void main(String[] args) {
 
+        Service service = getService();
+        service.test();
 
-        System.out.println(
-                ApplicationContext.getInstance().getBean(ProvinceRepository.class)
-                        .countAll()
-        );
-
-
+        System.out.println(service.heavyFunction());
+        System.out.println(service.heavyFunction());
     }
 
-    private static User getUser() {
-        return new User();
+    private static Service getService() {
+        return new ProxyServiceImpl(
+                new RealServiceImpl()
+        );
     }
 }
+
+interface Service {
+    void test();
+
+    String heavyFunction();
+}
+
+class RealServiceImpl implements Service {
+    @Override
+    public void test() {
+        System.out.println("inside service");
+    }
+
+    @Override
+    public String heavyFunction() {
+        System.out.println("in real heavyFunction");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return "Heavy";
+    }
+}
+
+@RequiredArgsConstructor
+class ProxyServiceImpl implements Service {
+
+    private final RealServiceImpl service;
+
+    private HeavyCash cash;
+
+    @Override
+    public void test() {
+        System.out.println("before");
+        service.test();
+        System.out.println("after");
+    }
+
+    @Override
+    public String heavyFunction() {
+        System.out.println("in proxy heavyFunction");
+        if (cash == null || Duration.between(LocalDateTime.now(), cash.lastCall).toMinutes() > 10) {
+            callAndCashHeavyFunction();
+        }
+        return cash.value;
+    }
+
+    private void callAndCashHeavyFunction() {
+        String data = service.heavyFunction();
+        cash = new HeavyCash();
+        cash.value = data;
+        cash.lastCall = LocalDateTime.now();
+    }
+
+    private static class HeavyCash {
+        private String value;
+        private LocalDateTime lastCall;
+    }
+}
+
