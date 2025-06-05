18c38c0928bacb439e77cd3d914876fda5cf1eb1
maxixking66 <maxixking66@gamil.com>
2025-06-05 09:47:37 +0330

    use Files class to write to file

--- src/main/java/ir/maktabsharif127/jpa/JpaApplication.java
+++ src/main/java/ir/maktabsharif127/jpa/JpaApplication.java
@@ -1,43 +1,20 @@
 package ir.maktabsharif127.jpa;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class JpaApplication {
     public static void main(String[] args) throws IOException {
 
-//        createFileAndWriteOnToIt();
-        readFile();
-    }
+        Files.write(
+                Path.of("note-2.txt"), "Mohsen Asgari\nAli alavi".getBytes(StandardCharsets.UTF_8)
+        );
 
-    private static void readFile() throws IOException {
-        File file = createFileIfNotExists("note.txt");
-        try (FileReader fileReader = new FileReader(file, StandardCharsets.UTF_8)) {
-            int read = fileReader.read();
-            while (read != -1) {
-                System.out.print((char) read);
-                read = fileReader.read();
-            }
-        }
-    }
+        Files.writeString(
+                Path.of("note-3.txt"), "Mohsen Asgari\nAli alavi"
+        );
 
-    private static void createFileAndWriteOnToIt() throws IOException {
-        File file = createFileIfNotExists("note.txt");
-        try (FileWriter fileWriter = new FileWriter(file, StandardCharsets.UTF_8, false)) {
-            fileWriter.write("mohsen asgari\nmohsen asgari");
-        }
-    }
-
-    private static File createFileIfNotExists(String fileName) throws IOException {
-        File file = new File(fileName);
-        if (!file.exists()) {
-            if (!file.createNewFile()) {
-                throw new RuntimeException();
-            }
-        }
-        return file;
     }
 }
