2b35362e3bc23d9c8d126914ddc6ae2c1c25c922
maxixking66 <maxixking66@gamil.com>
2025-06-05 09:19:35 +0330

    add file class example

--- src/main/java/ir/maktabsharif127/jpa/JpaApplication.java
+++ src/main/java/ir/maktabsharif127/jpa/JpaApplication.java
@@ -1,99 +1,21 @@
 package ir.maktabsharif127.jpa;
 
-import lombok.RequiredArgsConstructor;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
+import java.io.File;
+import java.io.IOException;
 
 public class JpaApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Service service = getService();
+        useFileClass();
 
-        long start = System.currentTimeMillis();
-        service.lightFunction();
-        service.lightFunction();
-        long end = System.currentTimeMillis();
-        System.out.println(end - start);
     }
 
-    private static Service getService() {
-        return new ProxyServiceImpl(
-                new RealServiceImpl()
-        );
+    private static void useFileClass() throws IOException {
+        File file = new File("note.txt");
+        System.out.println("exists(): " + file.exists());
+        System.out.println("getPath(): " + file.getPath());
+        System.out.println("getAbsolutePath(): " + file.getAbsolutePath());
+        System.out.println("createNewFile(): " + file.createNewFile());
+        System.out.println("exists(): " + file.exists());
     }
 }
-
-interface Service {
-    void test();
-
-    String heavyFunction();
-
-    void lightFunction();
-}
-
-class RealServiceImpl implements Service {
-    @Override
-    public void test() {
-        System.out.println("inside service");
-    }
-
-    @Override
-    public String heavyFunction() {
-        System.out.println("in real heavyFunction");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        return "Heavy";
-    }
-
-    @Override
-    public void lightFunction() {
-        System.out.println("light function");
-        heavyFunction();
-    }
-}
-
-@RequiredArgsConstructor
-class ProxyServiceImpl implements Service {
-
-    private final RealServiceImpl service;
-
-    private HeavyCash cash;
-
-    @Override
-    public void test() {
-        System.out.println("before");
-        service.test();
-        System.out.println("after");
-    }
-
-    @Override
-    public String heavyFunction() {
-        System.out.println("in proxy heavyFunction");
-        if (cash == null || Duration.between(LocalDateTime.now(), cash.lastCall).toMinutes() > 10) {
-            callAndCashHeavyFunction();
-        }
-        return cash.value;
-    }
-
-    @Override
-    public void lightFunction() {
-        service.lightFunction();
-    }
-
-    private void callAndCashHeavyFunction() {
-        String data = service.heavyFunction();
-        cash = new HeavyCash();
-        cash.value = data;
-        cash.lastCall = LocalDateTime.now();
-    }
-
-    private static class HeavyCash {
-        private String value;
-        private LocalDateTime lastCall;
-    }
-}
-
